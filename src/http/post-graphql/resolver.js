dacac5c9a1d84c4fe19e2df0ab5e25139e93a5a3
Anita C <huilong@gmail.com>
2021-01-06 01:36:28 -0800

    edit sql

--- src/http/post-graphql/resolver.js
+++ src/http/post-graphql/resolver.js
@@ -1,5 +1,4 @@
 const resolvers = {
-    Query: {
         check: () => 'All good',
         header() {
             return resolveHeader()
@@ -16,13 +15,13 @@ const resolvers = {
         contact() {
             return resolveContact()
         }
-    }
+    
 }
 
 const resolveHeader = () => {
     return {
         name: "Anita",
-        avatarImg: "https://raw.githubusercontent.com/petruki/rogerportfolio/master/src/images/avatar.png",
+        avatarImg: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/avatar.jpg?raw=true",
         headerTagline: [
           "Full-stack Developer"
         ],
@@ -36,7 +35,7 @@ const resolveWork = () => {
     return [
         {
             id: 1,
-            title: "Smart Parking Cloud Service",
+            title: "Smart Parking Cloud Service and Front-end Page",
             para:
                 "It is a cloud-based, multiple device parking payments software platform that enables people to park conveniently " +
                 "while providing lot owners and managers with real-time data and enforcement tools."+
@@ -46,42 +45,34 @@ const resolveWork = () => {
         },
         {
             id: 2,
-            title: "Smart Parking Front-end",
-            para:
-                "Develop Internal HR System, Budget System, Analysis and Report based on SpringMVC, Hibernate",
-            imageSrc: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/work/paku.png?raw=true",
-            url: "https://github.com/f228476653/parking_ui"
-        },
-        {
-            id: 3,
             title: "TPE Airport App Backend",
             para:
                 "Use Django REST framework to build APIs as backend for Airport mobile apps ",
-            imageSrc: "https://raw.githubusercontent.com/petruki/rogerportfolio/master/src/images/work/trackerforce.png",
+            imageSrc: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/work/airport_big_logo.png?raw=true",
             url: "https://www.taoyuan-airport.com/main_en/docdetail.aspx?uid=437&pid=437&docid=165"
         },
         {
-            id: 4,
+            id: 3,
             title: "Internal Protocal System",
             para:
                 "Develop Internal HR System, Budget System, Analysis and Report based on SpringMVC, Hibernate",
-            imageSrc: "",
+            imageSrc: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/work/iisi.png?raw=true",
             url: "https://www.iisigroup.com/"
         },
         {
-            id: 5,
+            id: 4,
             title: "WordPress Forum",
             para:
                 "A forum for chinese international students in Vancouver",
-            imageSrc: "",
+            imageSrc: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/work/wordpress.png?raw=true",
             url: "https://studentbb.com/"
         },
         {
-            id: 6,
+            id: 5,
             title: "Other Projects",
             para:
                 "From granular APIs to Full-stack applications. Find out more about my recent projects and experiments on my GitHub page.",
-            imageSrc: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/work/github.png",
+            imageSrc: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/work/github.png?raw=true",
             url: "https://github.com/f228476653?tab=repositories"
         }
     ]
@@ -99,7 +90,7 @@ const resolveAbout = () => {
             "As a experienced developer, I was able to use and learn several other skills to deliver the best solution through a concise roadmap that would lead to results.",
         aboutParaFour:
             "Throughout this exciting journey as a Software Engineer, I also have other interests that unconsciously go hand in hand, such as culinary, handicraft. " ,
-        aboutImage: "https://raw.githubusercontent.com/petruki/rogerportfolio/master/src/images/roger.png"
+        aboutImage: ""
     }
 }
 
@@ -107,7 +98,7 @@ const resolveSkill = () => {
     return [
         {
             id: 1,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/java.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/java.png?raw=true",
             para:
                 "Started using Java from the beginning of my career. I have developed software of many kind, " +
                 "from desktop, web, mobile and webservices, APIs and much more.",
@@ -125,7 +116,7 @@ const resolveSkill = () => {
             ]
         },{
             id: 2,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/python.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/python.png?raw=true",
             para:
             "Python has been the chosen runtime for some of my recent projects. " +
             "I have written APIs, web crawlers and libraries in conjunction with several different frameworks. " +
@@ -144,7 +135,7 @@ const resolveSkill = () => {
         },
         {
             id: 3,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/angular.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/angular.png?raw=true",
             para:
                 "For my front-end development framework of choice, Angular 6 was used to build Parking Platform, " +
                 "a project that interfaces with Parking Platform API and provides a simple yet robust UI. " +
@@ -164,34 +155,37 @@ const resolveSkill = () => {
         },
         {
             id: 4,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/nodejs.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/nodejs.png?raw=true",
             para:
                 "Nodejs has been the chosen runtime for some of my recent open-source projects. " +
                 "I have written APIs, web crawlers and libraries in conjunction with several different frameworks. " +
-                "Just to mention a few, express, express-graphql, socket.io, cheerio, supertest, jest, sinon, mocha, and much more.",
+                "Just to mention a few, express, express-graphql, jest, sinon, mocha, and much more.",
             references: [
                 {
-                    title: 'Switcher API',
-                    description: 'Complete and robust API for toggling features on the fly.',
-                    link: 'https://github.com/switcherapi/switcher-api'
-                },
-                {
-                    title: 'Switcher Load Balance',
-                    description: 'A simple yet functional load balance.',
-                    link: 'https://github.com/switcherapi/switcher-load-balance'
+                    title: 'Shopping WebSite Backend API',
+                    description: 'Shopping Cart using NodeJS with Express and MongoDB.',
+                    link: 'https://github.com/f228476653/Shopping-site'
                 }
             ]
         },
         {
             id: 5,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/oracle.png",
+            img: "https://github.com/f228476653/anita-portfolio/blob/master/src/images/skills/react.png?raw=true",
             para:
-                "Hands-on experience working for 8 years with Oracle solutions. " +
-                "Developed an enterprise solution using Oracle 11g as DBMS, Oracle Coherence for in-memory data caching and Oracle RTD as our machine learning component."
+                "React makes it painless to create interactive UIs. " +
+                "Build encapsulated components that manage their own state, then compose them to make complex UIs."
+                ,
+            references: [
+                {
+                        title: 'Twitter-clone',
+                        description: 'Fully functioning Front-End for a Twitter clone with React' ,
+                        link: 'https://github.com/f228476653/CSIS3275001'
+                }
+            ]
         },
         {
             id: 6,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/mongodb.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/mongodb.png?raw=true",
             para:
                 "When the goal is to develop a concise and granular APIs which scales horizontally, " +
                 "MongoDB is my NoSQL DB of choice. Using it to produce aggregation analysis or " +
@@ -199,7 +193,7 @@ const resolveSkill = () => {
         },
         {
             id: 7,
-            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/git.png",
+            img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/master/src/images/skills/git.png?raw=true",
             para:
                 "Not only a version controller, but part of the process of creating software which each one of them might have different approaches. " +
                 "As we have worked mostly with open-source projects, it requires a high level of code verification and " +
@@ -213,12 +207,12 @@ const resolveContact = () => {
         contactSubHeading: "Let's connect!",
         social: [
             {
-                img: "https://raw.githubusercontent.com/petruki/rogerportfolio/65b303bb15d08f3f43c0d5433e8d51a51fa603a7/src/images/linkedin.svg",
-                url: "https://www.linkedin.com/in/rogerit"
+                img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/b7c89ce502a9bea58708408d974717aa511ad431/src/images/linkedin.svg",
+                url: "https://www.linkedin.com/in/anita-chang/"
             },
             {
-                img: "https://raw.githubusercontent.com/petruki/rogerportfolio/65b303bb15d08f3f43c0d5433e8d51a51fa603a7/src/images/github.svg",
-                url: "https://github.com/petruki",
+                img: "https://raw.githubusercontent.com/f228476653/anita-portfolio/b7c89ce502a9bea58708408d974717aa511ad431/src/images/github.svg",
+                url: "https://github.com/f228476653",
             }
         ]
     }
